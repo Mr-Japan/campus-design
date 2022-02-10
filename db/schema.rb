22f116390344e1c291954e56b7a0efdb328ff688
EC2 Default User <ec2-user@ip-172-31-25-190.us-east-2.compute.internal>
2022-02-10 09:58:43 +0000

    管理者画面＋管理者ログイン画面の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_065253) do
+ActiveRecord::Schema.define(version: 2022_02_09_105000) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
